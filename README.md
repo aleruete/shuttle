e9230202897c81de5a9d2b02953b433bd2fc58d7
Dan Otto <54476948+dantheftotto@users.noreply.github.com>
2020-02-03 19:40:57 -0500

    created weather3 and timeseries3
    * Pulling to Dev (#8)
    
    * added webscrape1, geyser2, and timeseries2
    
    * added a pokemon easter egg
    
    * small addition to README file
    
    * ignoring the README.html file
    
    * correcting a typo in the README
    
    * webscrape 1 created
    
    * cleaning up webscrape1 file and adding some comments to it
    
    * created geyser2
    
    * small cleanup to timeseries1
    
    * created timeseries2
    
    * comment out the scrolling text; plan to use later
    
    * do not need topics_module at this time
    
    * Dev (#7)
    
    * added a pokemon easter egg
    
    * small addition to README file
    
    * ignoring the README.html file
    
    * correcting a typo in the README
    
    * webscrape 1 created
    
    * cleaning up webscrape1 file and adding some comments to it
    
    * created geyser2
    
    * small cleanup to timeseries1
    
    * created timeseries2
    
    * comment out the scrolling text; plan to use later
    
    * do not need topics_module at this time
    
    * geyser_test not needed
    
    * cleaned up library calls
    
    * changed github pic and added small comments
    
    * added small comments
    
    * required dygraphs and made titles reactive to button
    
    * small cleanup
    
    * cleaned up weather app code and other old code
    
    * created weather3
    
    * created timeseries3
    
    * removed newTA from chart and cleaned up some code
    
    * inserting install.packages code and tz list
    
    * minor formatting
    
    * update readme

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Shuttle
-A place for getting excited and curious about data science!
+A place for getting excited and curious about data science! For all the videos that go along with the lessons, visit [Shuttle Data Science](https://www.youtube.com/channel/UCHIge2lulmLXhEhWpajOT3Q) on YouTube.
 
 **Version 0.1**
-This is a very early version of this project.
+This is a very early version of this project. All suggestions for improvements are welcome. :)
 
 ---
 
 ## Contact
-Dan Otto
-ShuttleDataScience@gmail.com
+Dan Otto ShuttleDataScience@gmail.com
 
--- global.R
+++ global.R
@@ -1,3 +1,6 @@
+# install.packages(c("shiny", "shinydashboard", "shinyjs", "tidyverse","rvest",
+#                    "tidyquant","dygraphs","xts","DT","xml2","lubridate"))
+
 # Loading all the packages needed
 library(shiny)
 library(shinydashboard)
@@ -9,20 +12,21 @@ library(rvest)
 source("login.R")
 source("home/home.R")
 source("home/googleRSS.R")
-#Lesson 1 Files
+# Lesson 1 Files
 source("lesson1/geyser1.R")
 source("lesson1/weather1.R")
 source("lesson1/datatable1.R")
 source("lesson1/timeseries1.R")
 source("lesson1/webscrape1.R")
-#Lesson 2 Files
+# Lesson 2 Files
 source("lesson2/geyser2.R")
 source("lesson2/datatable2.R")
 source("lesson2/timeseries2.R")
-#Lesson 3 Files
+# Lesson 3 Files
 source("lesson3/weather3.R")
 
 # Sets the time zone
+# Find your timezone here https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 Sys.setenv(TZ="America/New_York")
 
 # A very simple username/password table
