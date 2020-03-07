46398f19584c5ed15d5c0546cae55557353ddcca
Dan Otto <shuttledatascience@gmail.com>
2020-03-07 21:31:57 +0000

    streamlined login screen variables and formatting

--- login.R
+++ login.R
@@ -6,52 +6,43 @@ loginUI <- function(id) {
 
 login <- function(input, output, session) {
   
-  credentials <- reactiveValues(user_login = FALSE,
-                                user = NULL)
+  credentials <- reactiveValues(access = FALSE,
+                                name = NULL)
   
-output$login_ui <- renderUI({
-  
-  if(credentials$user_login == TRUE) return(NULL)
-  
-  fluidPage(
-    div(style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
-        wellPanel(tags$head(
-          tags$link(rel = "stylesheet", type = "text/css",
-                    href = "style.css")),
-          h2("3.. 2.. 1..", class = "text-center", style = "padding-top: 0;"),
-          
-          tags$div(class = "text-center",
-                   tags$p("Hold on to your butts")
-                   ),
-          
-          textInput(session$ns("user_name"), "User Name:", value = user_github),
-          
-          div(
-            style = "text-align: center;",
-            actionButton(session$ns("login_button"), "Launch", class = "btn-primary", style = "color: white;")
-          ),
-          
-          uiOutput(session$ns("login_error"))
-          
+  output$login_ui <- renderUI({
+    
+    if(credentials$access == TRUE) return(NULL)
+    
+    fluidPage(
+      div(style = 'width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;',
+          wellPanel(
+            h2("3.. 2.. 1..", style = 'color:#1c1e21; font-weight: bold; text-align: center; padding-top: 0;'),
+            
+            textInput(session$ns("user_name"), "User Name:", value = user_github),
+            
+            div(style = "text-align: center;",
+                actionButton(session$ns("login_button"), "Launch", class = 'btn-primary', style = 'color: white;')
+            ),
+            
+            uiOutput(session$ns("login_error"))
           )
-        )
-  )
-  
-})
+      )
+    )
+  })
 
 
 observeEvent(input$login_button, {
   
-  Username <- input$user_name
+  # Username <- input$user_name
   
-  if (Username != user_github) {
+  if (input$user_name != user_github) {
     output$login_error <- renderUI({
-      p("Invalid username!", style = "color: red; font-weight: bold; padding-top: 5px;", class = "text-center")
+      p("Invalid username!", style = "color: red; font-weight: bold; padding-top: 5px;", style = 'font-weight: bold; text-align: center;')
     })
   } else {
     
-    credentials$user_login <- TRUE
-    credentials$user <- Username
+    credentials$access <- TRUE
+    credentials$name <- input$user_name
   }
   
 })
